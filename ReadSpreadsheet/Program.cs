f9179c3e3d26d0deeb2a99eeeef05dacd3ad6136
ricardocaiuba <ricardo.caiuba@gmail.com>
2021-08-25 20:08:40 -0300

    Initial Commit

---
+++ ReadSpreadsheet/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using ClosedXML.Excel;
+
+namespace ReadSpreadsheet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var xls = new XLWorkbook(@"D:\Plano Contábil.xlsx");
+            var planilha = xls.Worksheets.First(w => w.Name == "Planilha1");
+            var totalLinhas = planilha.Rows().Count();
+
+            // primeira linha é o cabecalho
+            for (int l = 2; l <= totalLinhas; l++)
+            {
+                var conta = planilha.Cell($"A{l}").Value.ToString();
+                var descricao = planilha.Cell($"B{l}").Value.ToString();
+                var reduzido = planilha.Cell($"C{l}").Value.ToString();
+                var grau = planilha.Cell($"D{l}").Value.ToString();
+                Console.WriteLine($"{conta} | {descricao} | {reduzido} | {grau}");
+            }
+        }
+    }
+}
